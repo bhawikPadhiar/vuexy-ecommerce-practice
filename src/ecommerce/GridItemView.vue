4d4f3dd17d63e0aa49b80a41e9c1cfe58cb73709
bhawik <bhawikp@zignuts.com>
2023-03-10 18:53:06 +0530

    first commit

--- src/ecommerce/GridItemView.vue
+++ src/ecommerce/GridItemView.vue
@@ -1,42 +1,76 @@
+<!-- =========================================================================================
+	File Name: GridItemView.vue
+	Description: Item Component - Grid VIew
+	----------------------------------------------------------------------------------------
+	Item Name: Vuexy - Vuejs
+========================================================================================== -->
 <template>
-<div>
-    <div class="vx-row">
-	<div class="vx-col w-full sm:w-1/2 lg:w-1/3 mb-base">
-		<vx-card class="overflow-hidden">
-			<div slot="no-body">
-		<template>
-			<img src="/img/laravel.png" alt="">
-		</template>
-		<div class="item-details px-4">
+		<!-- <div class="vx-row"> -->
+        <!-- <div class="grid-view-item vx-col" > -->
+			<!-- <div class=" vx-row match-height" >
+				<div class="vx-col lg:w-1/3 sm:w-1/2 w-full" v-for="items in dataList" :key="items.id"> -->
+			<vx-card class="grid-view-item mb-base overflow-hidden mt-3" v-on="$listeners">	
+				<template slot="no-body">
+					
+					<div class="item-img-container bg-white h-64 flex items-center justify-center mb-4 cursor-pointer">
+						<img :src="items.img" class='grid-view-img px-4' alt="">
+					</div>
+					<div class="item-details px-4">
 
-    <!-- RATING & PRICE -->
-    <div class="flex justify-between items-center">
-	<div class="text-warning border border-solid border-warning flex py-1 px-2 rounded">
-		<span class="text-sm mr-1">5</span>
-		<feather-icon icon="StarIcon" svgClasses="h-4 w-4" />
-	</div>
-	<h6 class="font-bold">$85</h6>
-    </div>
+						<!-- RATING & PRICE -->
+						<div class="flex justify-between items-center">
+							<div class="text-warning border border-solid border-warning flex py-1 px-2 rounded">
+								<span class="text-sm mr-1">5</span>
+								<feather-icon icon="StarIcon" svgClasses="h-4 w-4" />
+							</div>
+							<h6 class="font-bold">{{ items.price }}</h6>
+						</div>
 
-		<div margin="auto" class="my-4">
-           <h5 class="truncate font-semibold mb-1 hover:text-primary cursor-pointer" >Trial NameText</h5>
-         <p class="item-description truncate text-sm">Trial Description</p>
-      </div>
-	</div>
-	</div>
-	</vx-card>
-</div>
-<div class="vx-col w-full sm:w-1/2 lg:w-1/3 mb-base">
-		<vx-card class="overflow-hidden">
-			<div slot="no-body">
-		<template>
-			<img src="/img/laravel.png" alt="">
-		</template>
-		</div>
-	</vx-card> 
-</div>
-</div>
-</div>
+						<div class="my-4">
+							<h5 class="truncate font-semibold mb-1 hover:text-primary cursor-pointer">{{ items.name }}</h5>
+							<p class="item-description truncate text-sm">Trial Description</p>
+						</div>
+					</div>
+					<slot name="action-buttons"></slot>
+				</template>
+			</vx-card>
+		
+		<!-- </div>
+		
+	</div> -->
 </template>
 <script>
+
+
+export default{
+	props:['items'],
+	data(){
+		return{
+	item:[],
+	
+		}
+	},
+
+	
+
+}
 </script>
+<style lang="scss">
+.grid-view-item {
+	.grid-view-img {
+		max-width: 100%;
+		max-height: 100%;
+		width: auto;
+		transition: .35s;
+	}
+
+	&:hover {
+		transform: translateY(-5px);
+		box-shadow: 0px 4px 25px 0px rgba(0, 0, 0, .25);
+
+		.grid-view-img {
+			opacity: 0.9;
+		}
+	}
+}
+</style>
--- src/views/Page2.vue
+++ src/views/Page2.vue
@@ -1,31 +1,129 @@
+<!-- =========================================================================================
+	File Name: Page2.vue
+	Description: Item Component - Main VIew
+	----------------------------------------------------------------------------------------
+	Item Name: Vuexy - Vuejs
+========================================================================================== -->
 <template>
 	<div>
-		<div class="vx-row">
-	<div class="vx-col w-full sm:w-1/2 lg:w-1/3 mb-base">
-		<vx-card class="overflow-hidden">
-			<div slot="no-body">
-		<template>
-			<img src="/img/laravel.png" alt="">
-		</template>
-		<div class="item-details px-4">
+		<!-- <div> -->
+			<!-- ITEM VIEW - GRID/LIST -->
+			<div class="flex flex-wrap">
+				<feather-icon icon="GridIcon" @click="currentItemView = 'grid-view-item'"
+					class="p-2 shadow-drop rounded-lg d-theme-dark-bg cursor-pointer"
+					:svgClasses="{ 'text-primary stroke-current': currentItemView == 'grid-view-item' }"></feather-icon>
 
-<!-- RATING & PRICE -->
-<div class="flex justify-between items-center">
-	<div class="text-warning border border-solid border-warning flex py-1 px-2 rounded">
-		<span class="text-sm mr-1">5</span>
-		<feather-icon icon="StarIcon" svgClasses="h-4 w-4" />
-	</div>
-	<h6 class="font-bold">$85</h6>
-</div>
+				<feather-icon icon="ListIcon" @click="currentItemView = 'list-view-item'"
+					class="p-2 ml-4 shadow-drop rounded-lg d-theme-dark-bg cursor-pointer hidden sm:inline-flex"
+					:svgClasses="{ 'text-primary stroke-current': currentItemView == 'list-view-item' }"></feather-icon>
+			</div>
+		<!-- </div> -->
+	<div>
+		
+			<!-- GRID VIEW -->
+			<template v-if="currentItemView == 'grid-view-item'">
+				<!-- <div class="grid-view-item vx-row match-height" >
+					<div class="vx-col lg:w-1/3 sm:w-1/2 w-full" > -->
+						<div class=" vx-row match-height" >
+				<div class="vx-col lg:w-1/3 sm:w-1/2 w-full" v-for="items in dataList" :key="items.id">
+				<GridItemView :items="items">
+					
+					<!-- SLOT: ACTION BUTTONS -->
+					<template slot="action-buttons">
+						<div class="flex flex-wrap">
+							<!-- PRIMARY BUTTON: ADD TO WISH LIST -->
+							<div class="item-view-primary-action-btn p-3 flex flex-grow items-center justify-center cursor-pointer"
+								@click="toggleItemInWishList(items)">
+								<feather-icon icon="HeartIcon"
+									:svgClasses="[{	'fill-current text-danger': isInWishList(items.id)}, 'h-4 w-4']" />
+								<span class="text-sm font-semibold ml-2">WISHLIST</span>
+							</div>
 
-		<div margin="auto" class="my-4">
-           <h5 class="truncate font-semibold mb-1 hover:text-primary cursor-pointer" >Trial NameText</h5>
-         <p class="item-description truncate text-sm">Trial Description</p>
-      </div>
+							<!-- PRIMARY BUTTON: ADD-TO/VIEW-IN CART -->
+
+							<div class="item-view-secondary-action-btn bg-primary p-3 flex flex-grow items-center justify-center text-white cursor-pointer"
+								@click="cartButtonClicked(items.id)">
+								<feather-icon icon="ShoppingBagIcon" svgClasses="h-4 w-4" />
+								<span class="text-sm font-semibold ml-2" v-if="isInCart(items.id)">VIEW IN CART</span>
+								<span class="text-sm font-semibold ml-2" v-else>ADD TO CART</span>
+							</div>
+						</div>
+					</template>
+				</GridItemView>
+				<!-- </div> -->
+				<!-- </div> -->
+			</div>
+		
 	</div>
+			</template>
+		</div>
+
 	</div>
-	</vx-card>
-</div>
-</div>
-</div>
 </template>
+<script>
+import GridItemView from '../ecommerce/GridItemView'
+import axios from '../axios'
+
+export default {
+	//props:['dataList'],
+
+	components: {
+		GridItemView
+	},
+	data() {
+		return {
+			currentItemView: 'grid-view-item',
+		//	items:this.dataList,
+		dataList:[]
+		}
+	},
+	methods: {
+		toggleItemInWishList() {
+			//console.log(param);
+			//this.isInWishList(param)
+		},
+		isInWishList() {
+			
+			
+		},
+		cartButtonClicked(param) {
+			console.log(param)
+		},
+		isInCart(param) {
+			console.log(param)
+		},
+	
+		async getData(){
+			await axios.get('products').then(res => {
+				let arr = res.data;
+				let datalist1 = []
+				arr.forEach(element => {
+				datalist1.push(element);
+			});	
+			this.dataList=datalist1
+			console.log(this.dataList,'datalist')
+				
+			})
+		},
+		
+	},
+
+	created(){
+		this.getData()
+	}
+
+	
+}
+
+</script>
+<style>
+.item-view-primary-action-btn {
+	color: #2c2c2c !important;
+	background-color: #f6f6f6;
+	min-width: 50%;
+}
+
+.item-view-secondary-action-btn {
+	min-width: 50%;
+}
+</style>
